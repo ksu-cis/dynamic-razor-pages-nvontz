4cfeb5b68ba3a64fe0a115c8a0cb2c4b9c44e854
Nathan Vontz <nvontz@ksu.edu>
2020-04-22 14:56:17 -0500

    done

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -24,6 +24,16 @@ namespace Movies
                 string json = file.ReadToEnd();
                 movies = JsonConvert.DeserializeObject<List<Movie>>(json);
             }
+
+            HashSet<string> genreSet = new HashSet<string>();
+            foreach (Movie movie in movies)
+            {
+                if (movie.MajorGenre != null)
+                {
+                    genreSet.Add(movie.MajorGenre);
+                }
+            }
+            genres = genreSet.ToArray();
         }
 
         /// <summary>
@@ -40,9 +50,140 @@ namespace Movies
         {
             List<Movie> results = new List<Movie>();
 
+            if (terms == null) return All;
+
             foreach(Movie movie in All)
             {
                 if(movie.Title != null && movie.Title.Contains(terms, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    results.Add(movie);
+                }
+            }
+
+            return results;
+        }
+
+        public static string[] MPAARatings
+        {
+            get => new string[]
+            {
+                "G",
+                "PG",
+                "PG-13",
+                "R",
+                "NC-17"
+            };
+        }
+        /// <summary>
+        /// Filters the provided collection of movies to filter
+        /// </summary>
+        /// <param name="movies"></param>
+        /// <param name="ratings"></param>
+        /// <returns></returns>
+        public static IEnumerable<Movie> FilterByMPAARating(IEnumerable<Movie> movies, IEnumerable<string> ratings)
+        {
+            if (ratings == null || ratings.Count() == 0) return movies;
+
+            List<Movie> results = new List<Movie>();
+            foreach(Movie movie in movies)
+            {
+                if (movie.MPAARating != null && ratings.Contains(movie.MPAARating))
+                {
+                    results.Add(movie);
+                }
+            }
+            return results;
+        }
+        /// <summary>
+        /// The genres represented in the database
+        /// </summary>
+        private static string[] genres;
+        /// <summary>
+        /// Gets the movie genres represented in the database
+        /// </summary>
+        public static string[] Genres => genres;
+
+        /// <summary>
+        /// Filters the collection by Genre
+        /// </summary>
+        /// <param name="movies"></param>
+        /// <param name="ratings"></param>
+        /// <returns></returns>
+        public static IEnumerable<Movie> FilterByGenre(IEnumerable<Movie> movies, IEnumerable<string> genre)
+        {
+            if (genre == null || genre.Count() == 0) return movies;
+
+            List<Movie> results = new List<Movie>();
+            foreach (Movie movie in movies)
+            {
+                if (movie.MajorGenre != null && genre.Contains(movie.MajorGenre))
+                {
+                    results.Add(movie);
+                }
+            }
+            return results;
+        }
+
+        public static IEnumerable<Movie> FilterByIMDBRating(IEnumerable<Movie> movies, double? min, double? max)
+        {
+            if (min == null && max == null) return movies;
+           
+            var results = new List<Movie>();
+
+            if(min == null)
+            {
+                foreach(Movie movie in movies)
+                {
+                    if (movie.IMDBRating <= max) results.Add(movie);
+                }
+                return results;
+            }
+
+            if (max == null)
+            {
+                foreach (Movie movie in movies)
+                {
+                    if (movie.IMDBRating >= min) results.Add(movie);
+                }
+                return results;
+            }
+            foreach(Movie movie in movies)
+            {
+                if(movie.IMDBRating >= min && movie.IMDBRating <= max)
+                {
+                    results.Add(movie);
+                }
+            }
+
+            return results;
+        }
+
+        public static IEnumerable<Movie> FilterByRTRating(IEnumerable<Movie> movies, double? min, double? max)
+        {
+            if (min == null && max == null) return movies;
+
+            var results = new List<Movie>();
+
+            if (min == null)
+            {
+                foreach (Movie movie in movies)
+                {
+                    if (movie.RottenTomatoesRating <= max) results.Add(movie);
+                }
+                return results;
+            }
+
+            if (max == null)
+            {
+                foreach (Movie movie in movies)
+                {
+                    if (movie.RottenTomatoesRating >= min) results.Add(movie);
+                }
+                return results;
+            }
+            foreach (Movie movie in movies)
+            {
+                if (movie.RottenTomatoesRating >= min && movie.RottenTomatoesRating <= max)
                 {
                     results.Add(movie);
                 }
--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -10,12 +10,36 @@ namespace Movies.Pages
     public class IndexModel : PageModel
     {
         public IEnumerable<Movie> Movies { get; protected set; }
-        public string SearchTerms { get; set; }
 
-        public void OnGet()
+        [BindProperty]
+        public string SearchTerms { get; set; }
+        /// <summary>
+        /// The Filtered MPAA Ratings
+        /// </summary>
+        [BindProperty]
+        public string[] MPAARatings { get; set; }
+        [BindProperty]
+        public string[] Genres { get; set; }
+        
+        public double? IMDBMin { get; set; }
+
+        public double? IMDBMax { get; set; }
+
+        public double? RTMin { get; set; }
+
+        public double? RTMax { get; set; }
+
+
+
+        public void OnGet(double? IMDBMin, double? IMDBMax, double? RTMin, double? RTMax)
         {
-            String terms = Request.Query["SearchTerms"];
+            this.IMDBMin = IMDBMin;
+            this.IMDBMax = IMDBMax;
             Movies = MovieDatabase.Search(SearchTerms);
+            Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
+            Movies = MovieDatabase.FilterByGenre(Movies, Genres);
+            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
+            Movies = MovieDatabase.FilterByRTRating(Movies, RTMin, RTMax);
         }
 
     }

1dfb0c7117c4067c2b8350571c24de77f439aae8
Nathan Vontz <nvontz@ksu.edu>
2020-05-06 16:42:47 -0500

    added linq

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -33,6 +33,7 @@ namespace Movies.Pages
 
         public void OnGet(double? IMDBMin, double? IMDBMax, double? RTMin, double? RTMax)
         {
+            /*
             this.IMDBMin = IMDBMin;
             this.IMDBMax = IMDBMax;
             Movies = MovieDatabase.Search(SearchTerms);
@@ -40,7 +41,27 @@ namespace Movies.Pages
             Movies = MovieDatabase.FilterByGenre(Movies, Genres);
             Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
             Movies = MovieDatabase.FilterByRTRating(Movies, RTMin, RTMax);
-        }
+            */
+            Movies = MovieDatabase.All;
+            if (SearchTerms != null)
+            {
+               Movies = Movies.Where(movie => movie.Title != null && movie.Title.Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase));
+            }
+            if(MPAARatings != null && MPAARatings.Length != 0)
+            {
+                Movies = Movies.Where(movie => 
+                    movie.MPAARating != null && 
+                    MPAARatings.Contains(movie.MPAARating)
+                    );
+            }
 
+            if (Genres != null && Genres.Length != 0)
+            {
+                Movies = Movies.Where(movie =>
+                    movie.MajorGenre != null &&
+                    Genres.Contains(movie.MajorGenre)
+                    );
+            }
+        }
     }
 }
